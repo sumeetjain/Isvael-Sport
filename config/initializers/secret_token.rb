fa2047a6c6e369c0182e2fb7774409efe96aab94
Sumeet Jain <sumeet@sumeetjain.com>
2011-06-14 17:24:36 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Isvael::Application.config.secret_token = 'eea3a8058fe084e447933fbb86a1fa76f58251a73aae56865824f9aab575d8dc82de5c59c257aca8849a54104b7cff11b026e88a4d500d099bf812e3fc22549b'
